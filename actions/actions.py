6100d387460725b08e1bece795014477fc279bc5
Ansuman Mishra <ansumanmishra3.14@gmail.com>
2023-02-15 12:23:51 +0530

    changed

--- actions/actions.py
+++ actions/actions.py
@@ -18,41 +18,41 @@ from datetime import datetime
 import random
 
 
-# API_URL = "https://api.cricapi.com/v1/currentMatches"
-# API_KEY = "d0b73bf4-e8fc-4d3b-a9d8-8b8b6467c36b"
+API_URL = "https://api.cricapi.com/v1/currentMatches"
+API_KEY = "d0b73bf4-e8fc-4d3b-a9d8-8b8b6467c36b"
 
 
-# class ApiAction(Action):
-#     def name(self):
-#         return "action_match_news"
+class ApiAction(Action):
+    def name(self):
+        return "action_match_news"
 
-#     def run(self, dispatcher, tracker, domain):
-#         res = requests.get(API_URL + "?apikey=" + API_KEY + "&offset=0")
-#         if res.status_code == 200:
-#             result = res.json()
-#             num = random.randint(0, len(result['data']) - 1) - 1
-#             out_message = "Here some cricket quick info:\n1.The match between {} and {} was recently held and {} .".format(
-#                 result['data'][num]['teams'][0], result['data'][num]['teams'][1], result['data'][num]['status'])
+    def run(self, dispatcher, tracker, domain):
+        res = requests.get(API_URL + "?apikey=" + API_KEY + "&offset=0")
+        if res.status_code == 200:
+            result = res.json()
+            num = random.randint(0, len(result['data']) - 1) - 1
+            out_message = "Here some cricket quick info:\n1.The match between {} and {} was recently held and {} .".format(
+                result['data'][num]['teams'][0], result['data'][num]['teams'][1], result['data'][num]['status'])
 
-#             dispatcher.utter_message(out_message)
+            dispatcher.utter_message(out_message)
 
-#             out_message = "2.The next match was between {} and {} was recently held and {} .".format(
-#                 result['data'][num + 1]['teams'][0], result['data'][num + 1]['teams'][1],
-#                 result['data'][num + 1]['status'])
+            out_message = "2.The next match was between {} and {} was recently held and {} .".format(
+                result['data'][num + 1]['teams'][0], result['data'][num + 1]['teams'][1],
+                result['data'][num + 1]['status'])
 
-#             dispatcher.utter_message(out_message)
-
-#         return []
-
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
+            dispatcher.utter_message(out_message)
 
         return []
+
+# class ActionHelloWorld(Action):
+
+#     def name(self) -> Text:
+#         return "action_hello_world"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text="Hello World!")
+
+#         return []
